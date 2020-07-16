737b0d7d00bc96dcf3154beed09bdf725dcd8b1f
Tomisin Jenrola <tomisin.jenrola@securekey.com>
2020-07-16 09:44:36 -0400

    feat: added basic example for android
    Signed-off-by: Tomisin Jenrola <tomisin.jenrola@securekey.com>

---
+++ android/AriesDemo/app/src/test/java/com/github/trustbloc/ariesdemo/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright SecureKey Technologies Inc. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.github.trustbloc.ariesdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
